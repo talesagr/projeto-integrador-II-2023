d445da3e1b6cdc2cbe707995d2e5b1dce3263f92
Tales Aguiar <talesdd@outlook.com>
2023-06-02 21:15:20 -0300

    asa

--- projeto.integrador/projeto.integrador/src/main/java/br/edu/uricer/projeto/integrador/domains/Item.java
+++ projeto.integrador/projeto.integrador/src/main/java/br/edu/uricer/projeto/integrador/domains/Item.java
@@ -2,16 +2,22 @@ package br.edu.uricer.projeto.integrador.domains;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
-
+@NoArgsConstructor
 public class Item {
     int id;
     int weight;
     int value;
     String name;
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
 }
