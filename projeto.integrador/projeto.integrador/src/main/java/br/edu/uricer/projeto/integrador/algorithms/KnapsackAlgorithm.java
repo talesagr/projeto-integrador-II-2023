350376a0677a3b3c2767f28e0f11a31220d0d3a9
Tales Aguiar <talesdd@outlook.com>
2023-06-13 19:20:10 -0300

    finalizando o projeto para apresentacao. this is not the end

--- projeto.integrador/projeto.integrador/src/main/java/br/edu/uricer/projeto/integrador/algorithms/KnapsackAlgorithm.java
+++ projeto.integrador/projeto.integrador/src/main/java/br/edu/uricer/projeto/integrador/algorithms/KnapsackAlgorithm.java
@@ -5,7 +5,12 @@ import br.edu.uricer.projeto.integrador.domains.Item;
 import java.util.*;
 
 public class KnapsackAlgorithm {
-    public Map<Integer, String> knapsackDynamicProgramming(int[] weightOfAvailableItems, int[] valuesOfWeightOfAvailableItems, int availableItemsToPutInside, int targetValue, List<Item> itemList) {
+    public Map<Integer, String> knapsackDynamicProgramming(
+            int[] weightOfAvailableItems,
+            int[] valuesOfWeightOfAvailableItems,
+            int availableItemsToPutInside,
+            int targetValue,
+            List<Item> itemList) {
 
         if (availableItemsToPutInside <= 0 || targetValue <= 0) {
             return new HashMap<>();
@@ -43,4 +48,51 @@ public class KnapsackAlgorithm {
 
         return selectedItems;
     }
+
+    /*
+
+    //todo Implementar no futuro, pois assim faz mais sentido (removido o peso dos items)
+    public Map<Integer, String> knapsackDynamicProgramming(
+            int[] valuesOfAvailableItems,
+            int availableItemsToPutInside,
+            int targetValue,
+            List<Item> itemList) {
+
+        if (availableItemsToPutInside <= 0 || targetValue <= 0) {
+            return new HashMap<>();
+        }
+
+        int[][] m = new int[availableItemsToPutInside + 1][targetValue + 1];
+        boolean[][] selected = new boolean[availableItemsToPutInside + 1][targetValue + 1];
+
+        for (int i = 1; i <= availableItemsToPutInside; i++) {
+            for (int j = 1; j <= targetValue; j++) {
+                int valueWithoutItemI = m[i - 1][j];
+                if (valuesOfAvailableItems[i - 1] > j) {
+                    m[i][j] = valueWithoutItemI;
+                } else {
+                    int valueWithItemI = m[i - 1][j - valuesOfAvailableItems[i - 1]] + valuesOfAvailableItems[i - 1];
+                    if (valueWithItemI > valueWithoutItemI) {
+                        m[i][j] = valueWithItemI;
+                        selected[i][j] = true;
+                    } else {
+                        m[i][j] = valueWithoutItemI;
+                    }
+                }
+            }
+        }
+
+        Map<Integer, String> selectedItems = new HashMap<>();
+        int j = targetValue;
+        for (int i = availableItemsToPutInside; i >= 1 && j >= 1; i--) {
+            if (selected[i][j]) {
+                Item item = itemList.get(i - 1);
+                selectedItems.put(item.getId(), item.getName());
+                j -= valuesOfAvailableItems[i - 1];
+            }
+        }
+
+        return selectedItems;
+    }
+     */
 }
--- projeto.integrador/projeto.integrador/src/main/java/br/edu/uricer/projeto/integrador/services/ItemService.java
+++ projeto.integrador/projeto.integrador/src/main/java/br/edu/uricer/projeto/integrador/services/ItemService.java
@@ -19,17 +19,17 @@ public class ItemService {
     private static List<Item> createItemList() {
         List<Item> itemList = new ArrayList<>();
 
-        itemList.add(new Item(0,10, 80, "Cadeira de Madeira"));
-        itemList.add(new Item(1,20, 100, "Mesa de Centro"));
-        itemList.add(new Item(2, 30,140, "Cadeira de Ferro - Branca"));
-        itemList.add(new Item(3,40, 150, "Cadeira de Ferro - Preta"));
-        itemList.add(new Item(4,50, 200, "Mesa de Jantar de Madeira"));
-        itemList.add(new Item(5,60, 350, "Mesa de Jantar de Vidro"));
-        itemList.add(new Item(6,70, 1000, "Mesa de Jantar VIP"));
-        itemList.add(new Item(7,80, 500, "Cadeira VIP"));
-        itemList.add(new Item(8,90,3000,"Conjunto Mesa de Jantar VIP"));
-        itemList.add(new Item(10,80,50, "Luminária para mesa de centro"));
 
+        itemList.add(new Item(8,3000,3000,"Conjunto Mesa de Jantar VIP"));
+        itemList.add(new Item(6,1000, 1000, "Mesa de Jantar VIP"));
+        itemList.add(new Item(7,500, 500, "Cadeira VIP"));
+        itemList.add(new Item(5,350, 350, "Mesa de Jantar de Vidro"));
+        itemList.add(new Item(4,200, 200, "Mesa de Jantar de Madeira"));
+        itemList.add(new Item(3,150, 150, "Cadeira de Ferro - Preta"));
+        itemList.add(new Item(2, 140,140, "Cadeira de Ferro - Branca"));
+        itemList.add(new Item(1,100, 100, "Mesa de Centro"));
+        itemList.add(new Item(0,80, 80, "Cadeira de Madeira"));
+        itemList.add(new Item(10,50,50, "Luminária para mesa de centro"));
         return itemList;
     }
     public int[] getWeightOfItems() {
@@ -53,19 +53,5 @@ public class ItemService {
         }
 
         return valueOfItems;
-    }
-
-    public File saveItems(List<Item> newList) {
-        Gson gson = new Gson();
-        String json = gson.toJson(newList);
-        String filename = "items.json";
-
-        try (FileWriter fileWriter = new FileWriter(filename)) {
-            fileWriter.write(json);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return new File(filename);
     }
 }
